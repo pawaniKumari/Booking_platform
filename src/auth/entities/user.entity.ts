370b1739aa23f67ac353af60ccb0b568fc35c0d1
Pawani Kumari <pawanipushpakumari7@gmail.com>
2026-07-12 07:48:31 +0530

    implementing jwt authentication

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  passwordHash: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column({ nullable: true })
+  hashedRefreshToken: string;
+}
